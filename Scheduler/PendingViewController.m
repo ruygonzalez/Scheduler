de5a5e860eba21d6c7babb59883e9a4fa4656dce
Ruy Gonzalez <ruygonzalez@Ruys-MacBook.local>
2018-09-17 13:08:41 -0400

    datelabel now shows how much time remains until task is due, or if it's already due, shows overdue

--- Scheduler/PendingViewController.m
+++ Scheduler/PendingViewController.m
@@ -8,6 +8,7 @@
 
 #import "PendingViewController.h"
 #import "TaskTableViewCell.h"
+#import "Post.h"
 
 @interface PendingViewController ()
 
@@ -16,13 +17,31 @@
 @implementation PendingViewController
 
 // array with object data to be displayed in table
-NSArray *data;
+NSMutableArray *data;
 //NSString *CellIdentifier = @"identifiercustom";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    data = @[@"One", @"Two", @"Three"];
+    
+    // do a parse query for data
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+    //fetch data asynchronously
+    [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error){
+        if(posts != nil){
+            data = [NSMutableArray arrayWithArray:posts];
+            //data = [data arrayByAddingObjectsFromArray: posts];
+            NSLog(@"%d", posts.count);
+            NSLog(@"%d", data.count);
+            NSLog(@"query returned posts");
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+    
+    
+   //data = @[@"One", @"Two", @"Three"];
     self.tableView.dataSource = self;
     self.tableView.estimatedRowHeight = 117;
 //    self.tableView.rowHeight = UITableViewAutomaticDimension;
@@ -33,9 +52,25 @@ NSArray *data;
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     TaskTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"identifiercustom" forIndexPath:indexPath];
-    cell.descriptiontv.text = @"ayy";
-    cell.datelabel.text = @"January 12";
-    cell.prioritylabel.text = @"99";
+    NSLog(@"post about to be accessed");
+    Post *post = data[indexPath.row];
+    NSLog(@"Cell being generated");
+    cell.descriptiontv.text = post.description;
+    NSTimeInterval numberOfSecondsUntilSelectedDate = [post.duedate timeIntervalSinceNow];
+    if(numberOfSecondsUntilSelectedDate <= 0)
+        cell.datelabel.text = @"Overdue!";
+    else if(numberOfSecondsUntilSelectedDate < 60)
+        cell.datelabel.text = [NSString localizedStringWithFormat:@"%d seconds", (int) numberOfSecondsUntilSelectedDate];
+    else if(numberOfSecondsUntilSelectedDate < 3600)
+        cell.datelabel.text = [NSString localizedStringWithFormat:@"%d minutes", (int) numberOfSecondsUntilSelectedDate/60];
+    else if(numberOfSecondsUntilSelectedDate < 86400)
+        cell.datelabel.text = [NSString localizedStringWithFormat:@"%d hours and %d minutes", (int) numberOfSecondsUntilSelectedDate/3600, (int) numberOfSecondsUntilSelectedDate % 60];
+    else if(numberOfSecondsUntilSelectedDate < 172800)
+        cell.datelabel.text = [NSString localizedStringWithFormat:@"1 day, %d hours, and %d minutes", ((int) numberOfSecondsUntilSelectedDate % 86400) / 3600, (int) (( (int) numberOfSecondsUntilSelectedDate % 86400) % 3600) / 60];
+    else
+        cell.datelabel.text = [NSString localizedStringWithFormat:@"%d days, %d hours, and %d minutes", (int) numberOfSecondsUntilSelectedDate/86400, (int) ((int) numberOfSecondsUntilSelectedDate % 86400) / 3600, (int) ( ((int) numberOfSecondsUntilSelectedDate % 86400) % 3600) / 60];
+    //cell.datelabel.text = @"January 12";
+    cell.prioritylabel.text = [post.priority stringValue];
     
 //    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
 //    cell.textLabel.text = data[indexPath.row];
@@ -43,6 +78,8 @@ NSArray *data;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSLog(@"Data count:");
+    NSLog(@"%d", data.count);
     return data.count;
 }
 - (void)didReceiveMemoryWarning {
